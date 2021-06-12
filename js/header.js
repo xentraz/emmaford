40dafd3c5921f21c7efb9c71f4194eeaf129170e
Emma <69841693+xentraz@users.noreply.github.com>
2021-06-13 01:19:34 +0200

    responsive navigation complete

---
+++ js/header.js
@@ -0,0 +1,18 @@
+function openNav() {
+  document.querySelector('#overlay').style.height = "100%";
+  document.querySelector('.nav_bars').classList.add("hide");
+}
+
+function closeNav() {
+  document.querySelector('#overlay').style.height = "0%";
+  document.querySelector('.nav_bars').classList.remove('hide');
+}
+
+window.addEventListener('scroll', function(){
+  if(window.scrollY > 800){
+    document.querySelector('.nav_bars').classList.add('nav_bg');
+  } else {
+    document.querySelector('.nav_bars').classList.remove('nav_bg');
+  }
+});
+
